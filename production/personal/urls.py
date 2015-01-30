bbd16cd09de8e55afdb4b8646428cf5e8a5ec672
DrTeePot <tristanpotter@rogers.com>
2015-01-29 19:00:24 -0500

    added requirements and resume

--- production/personal/urls.py
+++ production/personal/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import patterns, url
 
 from production.personal import views
 
-
 urlpatterns = patterns('',  # TODO edit the license at the top of files
                        url(r'^$', views.index, name='personal_index'),
                        url(r'involvements/', views.involvements, name='personal_involvements'),
                        url(r'projects/', views.projects, name='personal_projects'),
-                       url(r'about/', views.about, name='personal_about')  # TODO Add this
                        )
--- production/personal/views.py
+++ production/personal/views.py
@@ -1,27 +1,22 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-
+from tristanpotter.settings import STATIC_PATH
 # Create your views here.
 
 
 def index(request):
     return render(request,
                   'personal/index.html',
-                  {})
+        {})
 
 
 def involvements(request):
     return render(request,
                   'personal/involvements.html',
-                  {})
+        {})
 
 
 def projects(request):
     return render(request,
                   'personal/projects.html',
-                  {})
-
-
-def about(request):
-    return render(request,
-                  'personal/about.html',
-                  {})
+        {})
